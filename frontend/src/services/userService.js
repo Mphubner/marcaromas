d4ee3fe09d43f9cfd7844321c55c5722c055ed57
Mphubner <mpereirah15@gmail.com>
2025-12-01 06:24:34 -0300

    fix: correct user profile service to use /users/ routes
    - Change userService.js to use /users/profile instead of /user/profile
    - Fixes 404 errors on profile page

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -2,11 +2,11 @@ import api from '../lib/api';
 
 export const userService = {
   getMyProfile: async () => {
-    const { data } = await api.get('/user/profile');
+    const { data } = await api.get('/users/profile');
     return data;
   },
   updateMyProfile: async (profileData) => {
-    const { data } = await api.patch('/user/profile', profileData);
+    const { data } = await api.patch('/users/profile', profileData);
     return data;
   },
 };
